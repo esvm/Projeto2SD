c63c7d291f955d8034cd76f754f7d8429d393e80
esvm <esvm@cin.ufpe.br>
2016-12-13 15:45:09 -0300

    Consertando os 21 erros

--- Control.v
+++ Control.v
@@ -10,50 +10,50 @@ parameter
 	addld = 3'b001, 
 	add = 3'b010,
 	div2 = 3'b011,
-	disp = 3'b100,
+	disp = 3'b100;
 
 //Código das instruções dos registradores
 parameter
-	hold = 2'b00;
-	load = 2'b01;
-	shiftr = 2'b10;
+	hold = 2'b00,
+	load = 2'b01,
+	shiftr = 2'b10,
 	reset = 2'b11;
 	
 always@(posedge clk) begin
 	case (Instrucao)
 		clrld: 
  		begin 
-			Tx <= 2'load;
-			Ty <= 2'reset;
-			Tz <= 2'reset; 
+			Tx <= load;
+			Ty <= reset;
+			Tz <= reset; 
 			Tula <= 0; 
 		 end
 		addld: 
 		 begin 
-			Tx <= 2'load;
-			Ty <= 2'load;
-			Tz <= 2'hold;
+			Tx <= load;
+			Ty <= load;
+			Tz <= hold;
 			Tula <= 0;
 		 end
 		add: 
 		 begin
-			Tx <= 2'hold;
-			Ty <= 2'load;
-			Tz <= 2'hold;
+			Tx <= hold;
+			Ty <= load;
+			Tz <= hold;
 			Tula <= 0;
 		 end
 		div2: 
 		 begin
-			Tx <= 2'hold;
-			Ty <= 2'shiftr;
-			Tz <= 2'hold;
+			Tx <= hold;
+			Ty <= shiftr;
+			Tz <= hold;
 			Tula <= 0;
 		 end
 		 disp:
 		 begin
-			Tx <= 2'reset;
-			Ty <= 2'reset;
-			Tz <= 2'load;
+			Tx <= reset;
+			Ty <= reset;
+			Tz <= load;
 			Tula <= 0;
 		 end
 	endcase 
